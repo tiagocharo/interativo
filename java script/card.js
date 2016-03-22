ff1dfe9ba7a4602a7d45f60351766538c4442e38
tiago <tiagofarocha@yahoo.com.br>
2016-03-21 22:36:15 -0300

    push to gh-pages

---
+++ java script/card.js
@@ -0,0 +1,13 @@
+var $cardColors = document.querySelectorAll('.card-colors');
+
+for (var index = 0; index < $cardColors.length;  index++) {
+	$cardColors[index].addEventListener('click', function() {
+		var $card = this.parentNode.parentNode.parentNode;
+
+		$card.dataset.color = this.dataset.color;
+		for (var position = 0; position < $cardColors.length; position++) {
+			$cardColors[position].classList.remove('isActive');
+		}
+		this.classList.add('isActive');
+	});
+};
